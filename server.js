d26b169b05861ddc6ee511dd37b410d510ff3522
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2019-11-07 15:56:32 +0700

    Setup server http and socket

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
+// get configuration
 const { SERVER_PORT } = require('./config');
 
 const app = express();
+const server = http.createServer(app);
+const socket = socketio(server);
 
-app.listen(SERVER_PORT, () => {
-  console.log(`Server is start on port ${SERVER_PORT}`);
+server.listen(SERVER_PORT, () => {
+  console.log(`Server is started on port ${SERVER_PORT}`);
 });
