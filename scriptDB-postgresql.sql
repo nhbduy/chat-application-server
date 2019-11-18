bf781bc2490d5e88a6c49736b09320015348d82a
Duy Nguyen <duy.nguyen@christinas.vn>
2019-11-18 23:34:01 +0700

    Add script db postgresql

---
+++ scriptDB-postgresql.sql
@@ -0,0 +1,36 @@
+CREATE TABLE TA_USERS (
+  id serial,
+  name varchar(100) NOT NULL UNIQUE,
+  online bool DEFAULT 0,
+  joined timestamp,
+  last_active timestamp,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE TA_ROOMS (
+  id serial,
+  name varchar(255) UNIQUE,
+  room_type int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE RE_USERS_ROOMS (
+  id serial,
+  user_id int NOT NULL,
+  room_id int NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES TA_USERS(id),
+  FOREIGN KEY (room_id) REFERENCES TA_ROOMS(id)
+);
+
+CREATE TABLE TA_MESSAGES (
+  id serial,
+  sender_id int,
+  room_id int,
+  content text,
+  created_at timestamp,
+  seen_by text,
+  PRIMARY KEY (id),
+  FOREIGN KEY (sender_id) REFERENCES TA_USERS(id),
+  FOREIGN KEY (room_id) REFERENCES TA_ROOMS(id)
+);
